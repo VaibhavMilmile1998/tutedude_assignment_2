65dd4f7a01e0eb6d00b5e6f432ec4eb0d23c5d55
Vaibhav Milmile <165415873+VaibhavMilmile1998@users.noreply.github.com>
2025-04-28 20:31:21 +0530

    Add files via upload

---
+++ Assignment_2_Task_1.py
@@ -0,0 +1,7 @@
+#Taking input number
+a=int(input("Enter the number: "))
+
+if(a%2 == 0):
+    print(a, " is a even number")
+else:
+    print(a, " is a odd number")
---
+++ Assignment_2_Task_2.py
@@ -0,0 +1,7 @@
+# Sum of Integers from 1 to 50 Using a Loop
+x=0
+#Calculates the sum of all integers in this range.
+for i in range(51):
+    x += i
+
+print("The sum of the numbers from 1 to 50 is ", x)
